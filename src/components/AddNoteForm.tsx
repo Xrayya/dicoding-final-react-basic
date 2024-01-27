63ec17686d9a22b9826175a6cdde83c468a53752
Xrayya <nur.alkhiyar@gmail.com>
2024-01-28 06:23:34 +0700

    feat(AddNoteForm): add fields

--- src/components/AddNoteForm.tsx
+++ src/components/AddNoteForm.tsx
@@ -1,8 +1,33 @@
 import React from "react";
+import { Input, TextArea } from "./Form";
 
 class AddNoteForm extends React.Component {
   render() {
-    return <div>Add Note Form</div>;
+    return (
+      <div className="fixed inset-0 m-auto w-1/2 h-fit">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <Input
+            type="text"
+            id="note-title"
+            name="note-title"
+            pattern="[A-Za-z]{3}"
+            title="Note Title"
+            label="Title"
+          />
+          <TextArea
+            id="note-body"
+            name="note-body"
+            title="Note Body"
+            label="Body"
+          />
+          <Input type="submit" />
+        </form>
+      </div>
+    );
   }
 }
 
