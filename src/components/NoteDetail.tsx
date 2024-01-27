b9e9c45aac54fc4779c0a85642134fc7ca39ad04
Xrayya <nur.alkhiyar@gmail.com>
2024-01-28 06:23:58 +0700

    refactor: use React.Component instead of Component

--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NoteDetail extends Component {
+class NoteDetail extends React.Component {
   render() {
     return <div>Note Detail</div>;
   }
