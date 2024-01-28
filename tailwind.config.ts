97bc482b16611ca38795c4031eb450142716ef40
Xrayya <nur.alkhiyar@gmail.com>
2024-01-28 16:01:33 +0700

    chore: remove light and grey extended colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,6 @@ export default {
       colors: {
         primary: "#90c46c",
         secondary: "#488dcf",
-        light: "#f8fafc",
-        lightVariant: "#e2e8f0",
-        gray: "#94a3b8",
-        grayVariant: "#64748b",
-        dark: "#020617",
-        darkVariant: "#0f172a",
         error: "#dc2626",
         warning: "#fde047",
         info: "#3b82f6",
