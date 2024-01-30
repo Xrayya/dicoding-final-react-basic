5373109802b74b2c33de65b89a9086058c15ac23
Xrayya <nur.alkhiyar@gmail.com>
2024-01-30 07:01:22 +0700

    feat(NoteItem): styling

--- src/components/NoteItem.tsx
+++ src/components/NoteItem.tsx
@@ -16,6 +16,7 @@ type NoteBodyProps = {
 
 type NoteButton = {
   label: string;
+  type?: "normal" | "error" | "warning" | "info";
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
@@ -39,14 +40,35 @@ export function Body({ body }: NoteBodyProps) {
   return <p>{body}</p>;
 }
 
-export function Button({ label, onClick }: NoteButton) {
-  return <button onClick={onClick}>{label}</button>;
+export function Button({ label, type, onClick }: NoteButton) {
+  let typeClass: string = "py-1 px-2 border-2 rounded-sm ";
+  switch (type) {
+    case "info":
+      typeClass += "border-info text-info";
+      break;
+    case "warning":
+      typeClass += "border-warning text-warning";
+      break;
+    case "error":
+      typeClass += "border-error text-error";
+      break;
+    case "normal":
+    default:
+      typeClass += "border-slate-900 text-slate-900";
+      break;
+  }
+
+  return (
+    <button className={typeClass} onClick={onClick}>
+      {label}
+    </button>
+  );
 }
 
 export function ButtonGroup({ children }: NoteButtonGroup) {
-  return <div>{children}</div>;
+  return <div className="p-0 flex flex-row justify-evenly rounded-sm overflow-hidden">{children}</div>;
 }
 
 export function Item({ children }: NoteItemProps) {
-  return <div>{children}</div>;
+  return <div className="min-w-20 min-h-20 w-fit">{children}</div>;
 }
