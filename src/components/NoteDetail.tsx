9eea7ef3c3f73e6dcb7a7b45dae0a8f38505e438
Xrayya <nur.alkhiyar@gmail.com>
2024-02-01 15:02:29 +0700

    feat(NoteDetail): styling

--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import { Note } from "@/model";
 
-class NoteDetail extends React.Component {
-  render() {
-    return <div>Note Detail</div>;
-  }
+type noteDetailProps = {
+  note?: Note;
+};
+
+function NoteDetail({ note }: noteDetailProps) {
+  return (
+    <div className="fixed inset-0 m-auto w-1/2 h-fit">
+      <div className="p-4 rounded-lg bg-slate-300">
+        <h2>{note?.title}</h2>
+        <div>
+          {note?.createdAt.toLocaleDateString("en-US", {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+          })}
+        </div>
+        <p>{note?.body}</p>
+      </div>
+    </div>
+  );
 }
 
 export default NoteDetail;
