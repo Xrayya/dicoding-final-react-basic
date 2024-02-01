659e25db97432d2e09d9c6f9032ab4055d018d1c
Xrayya <nur.alkhiyar@gmail.com>
2024-02-01 18:07:52 +0700

    feat(NoteDetail): more styling and add onClose prop

--- src/components/NoteDetail.tsx
+++ src/components/NoteDetail.tsx
@@ -2,14 +2,27 @@ import { Note } from "@/model";
 
 type noteDetailProps = {
   note?: Note;
+  onClose?: () => any;
 };
 
-function NoteDetail({ note }: noteDetailProps) {
+function NoteDetail({ note, onClose }: noteDetailProps) {
+  const handleClose = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (onClose) onClose();
+  };
+
   return (
-    <div className="fixed inset-0 m-auto w-1/2 h-fit">
-      <div className="p-4 rounded-lg bg-slate-300">
-        <h2>{note?.title}</h2>
-        <div>
+    <div className="fixed inset-0 m-auto w-1/2 h-3/4">
+      <div className="p-4 rounded-lg bg-slate-300 h-full">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-2xl font-bold text-secondary">{note?.title}</h2>
+          <button
+            className="flex justify-center items-center text-4xl rounded-md bg-slate-200 h-8 w-8 box-border"
+            onClick={handleClose}
+          >
+            ×
+          </button>
+        </div>
+        <div className="text-slate-600">
           {note?.createdAt.toLocaleDateString("en-US", {
             weekday: "long",
             day: "numeric",
@@ -17,7 +30,8 @@ function NoteDetail({ note }: noteDetailProps) {
             year: "numeric",
           })}
         </div>
-        <p>{note?.body}</p>
+        <hr className="border-slate-700" />
+        <p className="mt-8">{note?.body}</p>
       </div>
     </div>
   );
